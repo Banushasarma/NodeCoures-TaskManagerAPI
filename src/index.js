afb62556047f90997cc25b82ac782c73d1446d38
Banushasarma <sharmabanu03@gmail.com>
2021-07-16 22:44:38 +0530

    asdas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('./db/mongoose')
-const userRouter = require('./routers/User');
-const taskRouter = require('./routers/Task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express()
 const port = process.env.PORT
